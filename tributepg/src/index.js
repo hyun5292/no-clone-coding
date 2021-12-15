01ad817cbca323256e019061d03753fb12d7f16e
hyun5292 <tytyjacob@naver.com>
2021-12-16 02:16:25 +0900

    Tribute Page 완성
    클론 아니고 내가 직접 화면만 보고 만든 프로젝트
    반응형으로 핸드폰 사이즈 정도에 추가

--- tributepg/src/index.js
+++ tributepg/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './default.css';
 import './index.scss';
-import lifeData from "./db/lifeData.json";
+import lifeData from "./db/lifeData.json";  //Norman life Data JSON file
 
+//Norman life Data 뿌려주기
 const App = () => {
   return (
     <div>
-      <div class="cont-title">
+      <h3 class="cont-title">
             Here's a time line of Dr. Borlaug's life:
-      </div>
+      </h3>
       <ul>
         {lifeData.normanHistory.map((lData) => (
           <li key={lData.value}>
-            <span>{lData.year}</span>
-            - {lData.life}
+            <strong>{lData.year}</strong> - {lData.life}
           </li>
         ))}
       </ul>
